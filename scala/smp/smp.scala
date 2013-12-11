8af4cff4f2a6e4df47c7d1557b7b7cf3fd86abcc
Andrée Ekroth <andree.ekroth@gmail.com>
2013-12-11 22:32:08 +0100

    use Id instead of name, remove map

--- scala/smp/smp.scala
+++ scala/smp/smp.scala
@@ -2,39 +2,38 @@ object smp {
   import scala.collection.immutable.Queue
 
   type Name = String
+  type Id = Int
 
-  case class Man(name: Name, ws: Queue[Name], wife: Option[Name] = None) {
-    def next(): (Name, Man) = {
+  case class Man(id: Id, ws: Queue[Id], wife: Option[Id] = None) {
+    def next(): (Id, Man) = {
       val (w,nws) = ws.dequeue
       (w, copy(ws = nws))
     }
+
+    def out()(implicit names: Map[Id, Name]) = s"${names(id)} -- ${names(wife.get)}"
   }
 
-  case class Woman(name: Name, ms: Map[Name, Int], husband: Option[Man] = None) {
+  case class Woman(id: Id, ms: Vector[Int], husband: Option[Man] = None) {
     def propFrom(m: Man): (Option[Man], Woman) = {
-      val mlevel = ms(m.name)
-      val hlevel = husband map { x => ms(x.name) } getOrElse { mlevel + 1 }
+      val mlevel = ms.indexOf(m.id)
+      val hlevel = husband map { x => ms.indexOf(x.id) } getOrElse { mlevel + 1 }
 
       if (mlevel < hlevel) {
-        val h = m.copy(wife = Some(name))
+        val h = m.copy(wife = Some(id))
         val w = copy(husband = Some(h))
         (husband map { _.copy(wife = None) }, w)
       } else (Some(m), this)
     }
   }
 
-  def smp(ms: Queue[Man], wm: Map[Name, Woman]): List[Woman] = {
+  def smp(ms: Queue[Man], wm: Map[Id, Woman]): List[Woman] = {
     if (ms.isEmpty) wm.values.toList
     else {
       val (man, nms) = ms.dequeue
-      val (wname, tmpman) = man.next
-      val (nman, nwom) = wm(wname) propFrom tmpman
-      smp(nms ++ nman, wm.updated(nwom.name, nwom))
+      val (wid, tmpman) = man.next
+      val (nman, nwom) = wm(wid) propFrom tmpman
+      smp(nms ++ nman, wm.updated(nwom.id, nwom))
     }
-  }
-
-  def pretty(ws: List[Woman]): String = {
-    ws map { w => s"${w.name} -> ${w.husband.get.name}" } mkString ("\n")
   }
 
   /**
@@ -48,7 +47,7 @@ object smp {
     * }}}
     */
   object Friends {
-    val ms = Queue(
+/*    val ms = Queue(
       Man("Ross", Queue("Rachel", "Phoebe", "Monica")),
       Man("Chandler", Queue("Monica", "Rachel", "Phoebe")),
       Man("Joey", Queue("Rachel", "Phoebe", "Monica")))
@@ -56,7 +55,15 @@ object smp {
     val wm = Map(
       "Monica" -> Woman("Monica", Map("Chandler" -> 0, "Joey" -> 1, "Ross" -> 2)),
       "Phoebe" -> Woman("Phoebe", Map("Joey" -> 0, "Ross" -> 1, "Chandler" -> 2)),
-      "Rachel" -> Woman("Rachel", Map("Ross" -> 0, "Joey" -> 1, "Chandler" -> 2)))
+      "Rachel" -> Woman("Rachel", Map("Ross" -> 0, "Joey" -> 1, "Chandler" -> 2)))*/
   }
 
+  def read() {
+    /*import scala.io.Source
+
+    val (names, ms, ws) = Source.stdin().foldLeft(
+      (Map[Id, Name].empty, Queue[Man].empty, Map[Id, Woman].empty)) {
+      
+    }*/
+  }
 }
