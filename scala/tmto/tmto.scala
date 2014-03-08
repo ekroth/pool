e9ede2226789a679284f30c44e2865ca0ab4c820
Andrée Ekroth <andree.ekroth@gmail.com>
2014-03-08 23:51:42 +0100

    tmto: works, but many hash collisions

--- scala/tmto/tmto.scala
+++ scala/tmto/tmto.scala
@@ -1,20 +1,21 @@
 import java.security._
 
 package object tmto {
-  val ascii = (97.toChar, 122.toChar)
-  lazy val MD = MessageDigest.getInstance("SHA-1")
-  val Height = 15625
-  val Width = 1
+  lazy val MD = MessageDigest.getInstance("MD5")
+  val ascii = (65.toChar, 122.toChar)
+  val Height = 128
+  val Width = 128
 
   case class Hash(hash: String) {
+    /** Reduced hash */
     def reduced = {
-      val groups = (hash grouped (hash.length / 3)).toList match {
-        case x if (x.length == 3) => x
-        case x :: y :: z :: w :: Nil => x :: y :: (z + w) :: Nil
+      val groups = (hash grouped (hash.length / 2)).toList match {
+        case x if (x.length == 2) => x
+        case x :: y :: z :: Nil => x :: (y + z) :: Nil
       }
 
       val chars = groups map {
-        x => (ascii._1 + (x.sum % (ascii._2 - ascii._1))).toChar
+        x => (ascii._1 + (x.mkString.hashCode % (ascii._2 - ascii._1))).toChar
       }
 
       Pass(chars.mkString)
@@ -22,52 +23,59 @@ package object tmto {
   }
 
   case class Pass(pass: String) {
+    /** Hashed password */
     def hashed = Hash(MD.digest(pass.getBytes("UTF-8")).mkString)
   }
 
-  def row(start: Pass, size: Int) = {
-    val init = (List(start), start.hashed)
-    val end = (0 until size).foldLeft(init) { 
-      case ((ps, h), _) => {
-        val p = h.reduced
-        (p :: ps, p.hashed)
-      }
+  /** TMTO-table */
+  lazy val table = {
+    val passwords = {
+      val chars = ascii._1 until ascii._2
+      for {
+        x <- chars
+        y <- chars
+      } yield s"$x$y"
     }
 
-    (start, end._2, end._1)
-  }
+    assert {
+      val hs = passwords map { x => Pass(x) }
+      hs.length == hs.distinct.length
+    }
 
-  def matrix = {
-    val chars = ascii._1 until ascii._2
-    val combs = for {
-      x <- chars
-      y <- chars
-      z <- chars
-    } yield s"$x$y$z"
+    assert { passwords.length >= Height }
 
-
-    val indices = (0 until Height) map { _ * (combs.length / Height) }
-    (indices map {
+    val indices = (0 until Height) map { _ * (passwords.length / Height) }
+    val vs = indices map {
       x => {
-        val r = row(Pass(combs(x)), Width)
-        r._2 -> r._1
+        val pass = Pass(passwords(x))
+        val chain = Stream.iterate(pass.hashed) { _.reduced.hashed }
+        chain(Width - 1) -> pass
       }
-    }).toMap
-  }
-
-  def crack(table: Map[Hash, Pass], hash: Hash) = {
-    val combs = Math.pow(ascii._2 - ascii._1, 3)
-    println(s"Combinations: $combs, table: ${Width * Height}")
-
-    def crack(h: Hash, i: Int, row: Boolean): Pass = {
-      val pass = if (row) h.reduced else table.getOrElse(h, h.reduced)
-      val hazh = pass.hashed
-
-      if (i > Width * Height) Pass("Unknown")
-      else if (hazh == hash) pass
-      else crack(hazh, i + 1, row || table.contains(h))
     }
 
-    crack(hash, 0, false)
+    println(vs.mkString("\n"))
+
+    println(vs.length)
+    val map = vs.toMap
+    println(map.size)
+    map
+  }
+
+  /** Try to crack hash */
+  def crack(hash: Hash) = {
+    /** Search table row for Hash */
+    def crackrow(pass: Pass, end: Hash): Option[Pass] = pass.hashed match {
+      case `hash` => Some(pass)
+      case `end` => None
+      case h => crackrow(h.reduced, end)
+    }
+
+    /** Crack */
+    def crack(curr: Hash): Option[Pass] = table.get(curr) match {
+      case Some(pass) => crackrow(pass, curr)
+      case _ => crack(curr.reduced.hashed)
+    }
+
+    crack(hash)
   }
 }
