af37d9b2b02e5a35ca92511f6ba92fe2ccf163a9
Andrée Ekroth <andree.ekroth@gmail.com>
2014-10-07 22:25:03 +0200

    scala/collatz: simpler

--- scala/collatz/collatz.scala
+++ scala/collatz/collatz.scala
@@ -1,19 +1,20 @@
 object collatz {
+  import scala.collection.GenSeq
 
-  def all(n: Int): Set[Long] = {
+  def all(n: Int): GenSeq[Long] = {
     val xs = (1 to n).par
-    val seqs = xs map { seq(_) }
-    seqs reduce { _ ++ _ }
+    val combs = xs flatMap { seq(_) }
+    combs.distinct
   }
 
-  def seq(n: Long): Set[Long] = {
-    @annotation.tailrec def collatzAcc(n: Long, acc: Set[Long]): Set[Long] = {
+  def seq(n: Long): Seq[Long] = {
+    @annotation.tailrec def collatzAcc(n: Long, acc: Seq[Long]): Seq[Long] = {
       n match {
-        case 1 => acc + n
-        case _ if n % 2 == 0 => collatzAcc(n / 2, acc + n)
-        case _ => collatzAcc(n * 3 + 1, acc + n)
+        case 1 => acc :+ n
+        case _ if n % 2 == 0 => collatzAcc(n / 2, acc :+ n)
+        case _ => collatzAcc(n * 3 + 1, acc :+ n)
       }
     }
-    collatzAcc(n, Set.empty)
+    collatzAcc(n, Seq.empty)
   }
 }
